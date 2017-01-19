9cee0e3a49ac32444e008abe8a7ee394d140c0a4
Jenny Knuth <jknuth@n.io>
2017-01-19 16:35:39 -0700

    add 'undefined' is false tests, test class name mapping only, test interaction with sinon and simulate

--- specs/components/button.spec.js
+++ specs/components/button.spec.js
@@ -1,37 +1,50 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import { Button } from '../../components/button';
 
 describe('Button', () => {
   // buttons are required to have a theme object with at least a button class
   const theme = {
-    button: 'button',
-    primary: 'button--myVariant',
-    large: 'button--large',
-    custom: 'button--custom'
+    button: 'ahae',
+    primary: 'asdga',
+    large: 'ahfafe',
+    custom: 'agas',
+    one: 'rqeya',
+    two: 'ahfa'
   }
   it('should be a button with a button property in the theme', () => {
     const wrapper = shallow(<Button theme={theme} />);
-    expect(wrapper.hasClass('button')).to.be.true;
+    expect(wrapper.hasClass(theme.button)).to.not.be.an('undefined');
+    expect(wrapper.hasClass(theme.button)).to.be.true;
     expect(wrapper.type()).to.equal('button');
   });
   it('if it has a variant property, className should be the variant defined in the theme object', () => {
     const variant = 'primary';
     const wrapper = shallow(<Button variant={variant} theme={theme} />);
-    expect(wrapper.hasClass('button--myVariant')).to.be.true;
-    expect(wrapper.hasClass('button--variantThatIsNotInThemeObject')).to.be.false;
+    expect(wrapper.hasClass('undefined')).to.be.false;
+    expect(wrapper.hasClass(theme[variant])).to.be.true;
   });
   it('if it has a size property, className should map to the size defined in the theme object', () => {
     const size = 'large';
     const wrapper = shallow(<Button size={size} theme={theme} />);
-    expect(wrapper.hasClass('button--large')).to.be.true;
+    expect(wrapper.hasClass('undefined')).to.be.false;
+    expect(wrapper.hasClass(theme[size])).to.be.true;
   });
-  it('if it has a mod property, className should map to the mode name defined in the theme', () => {
+  it('if it has a mod property, className should map to the mod name defined in the theme', () => {
     const mod = 'custom';
     const wrapper = shallow(<Button mod={mod} theme={theme} />);
-    expect(wrapper.hasClass('button--custom')).to.be.true;
+    expect(wrapper.hasClass('undefined')).to.be.false;
+    expect(wrapper.hasClass(theme[mod])).to.be.true;
+  });
+  it('if it has a mod property that is an array, className should map to each mod name defined in the theme', () => {
+    const mod = ['one', 'two'];
+    const wrapper = shallow(<Button mod={mod} theme={theme} />);
+    expect(wrapper.hasClass('undefined')).to.be.false;
+    expect(wrapper.hasClass(theme[mod[0]])).to.be.true;
+    expect(wrapper.hasClass(theme[mod[1]])).to.be.true;
   });
   it('should return a link if it has an href property', () => {
     const href = 'html://www.example.com';
@@ -39,13 +52,9 @@ describe('Button', () => {
     expect(wrapper.type()).to.equal('a');
   });
   it('should have an onMouseUp property that is a function', () => {
-    const wrapper = shallow(<Button theme={theme} />);
-    expect(wrapper.props()).to.have.property('onMouseUp');
-    expect(wrapper.props().onMouseUp).to.be.a('function');
-  });
-  it('should have a ref property that is a function', () => {
-    const wrapper = shallow(<Button theme={theme} />);
-    expect(wrapper.node).to.have.property('ref');
-    expect(wrapper.node.ref).to.be.a('function');
+    const handleMouseUp = sinon.spy();
+    const wrapper = shallow(<Button theme={theme} onMouseUp={handleMouseUp} />);
+    wrapper.find('button').simulate('mouseUp');
+    expect(handleMouseUp.calledOnce).to.equal(true);
   });
 });

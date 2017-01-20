5f64922177b5c2d13b19ea133656f335ef817cc6
J. Holmes <jholmes@n.io>
2017-01-20 16:53:46 -0700

    updating text-input tests to pass...

--- specs/components/text-input.spec.jsx
+++ specs/components/text-input.spec.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import uniqueid from 'lodash.uniqueid';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import { TextInput } from '../../src/components/text-input';
 
-describe('TextInput', () => {
+describe('A TextInput', () => {
   // buttons are required to have a theme object with at least a button class
   const theme = {
-    textInput: 'textInput',
-  }
+    textInput: uniqueid(),
+  };
+
   it('should be a text input', () => {
     const wrapper = shallow(<TextInput theme={theme} />);
-    expect(wrapper.hasClass('textInput')).to.be.true;
-    expect(wrapper.type()).to.equal('input');
+    expect(wrapper.hasClass(theme.textInput)).to.be.true;
+    expect(wrapper.type()).to.equal('div');
   });
 });

5c3b17ca934013195589a6223dbae66e7158421f
Jenny Knuth <jknuth@n.io>
2017-02-16 15:23:37 -0700

    PROD-225 add tests for Twofold component

--- specs/components/twofold.spec.jsx
+++ specs/components/twofold.spec.jsx
@@ -12,70 +12,116 @@ describe('A Twofold', () => {
     active: uniqueid(),
   };
   const active = false;
-  // const child = uniqueid();
+  const child = uniqueid();
+  const detail = uniqueid();
 
   it('should have a twofold class.', () => {
-    const wrapper = shallow(<Twofold theme={theme} active={active}/>);
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
     expect(wrapper.hasClass(theme.twofold)).to.be.true;
   });
 
-  // it('should be an <a> tag.', () => {
-  //   const wrapper = shallow(<Twofold href={href} theme={theme} ></Twofold>);
-  //   expect(wrapper.type()).to.equal('a');
-  // });
-  //
-  // it('should render its children.', () => {
-  //   const wrapper = shallow(<Twofold href={href} theme={theme} >{child}</Twofold>);
-  //   expect(wrapper.contains(child)).to.be.true;
-  // });
-  //
-  // describe('with an icon', () => {
-  //   const variant = 'after';
-  //   const icon = uniqueid();
-  //
-  //   it('should have a data-icon property with a value of the icon.', () => {
-  //     const wrapper = shallow(<Twofold theme={theme} href={href} icon={icon} />);
-  //     expect(wrapper.prop('data-icon')).to.equal(icon);
-  //   });
-  //
-  //   describe('and a variant,', () => {
-  //     theme[variant] = uniqueid();
-  //
-  //     it('should have a class with that variant', () => {
-  //       const wrapper = shallow(<Twofold theme={theme} href={href} variant={variant} icon={icon} />);
-  //       expect(wrapper.hasClass(theme[variant])).to.be.true;
-  //     });
-  //   });
-  //
-  // });
-  //
-  // describe('with a className attribute,', () => {
-  //   const customClass = uniqueid();
-  //   theme[customClass] = uniqueid();
-  //
-  //   it('should contain that class.', () => {
-  //     const wrapper = shallow(<Twofold theme={theme} href={href} className={customClass} >{child}</Twofold>);
-  //     expect(wrapper.hasClass(theme[customClass])).to.be.true;
-  //   });
-  // });
-  //
-  // describe('with a theme with mods,', () => {
-  //   const theme = {
-  //     twofold: uniqueid(),
-  //     mod1: uniqueid(),
-  //     mod2: uniqueid(),
-  //   };
-  //
-  //   it('should support a single mod.', () => {
-  //     const wrapper = shallow(<Twofold theme={theme} mod="mod1" href={href} />);
-  //     expect(wrapper.hasClass(theme.mod1)).to.be.true;
-  //   });
-  //
-  //   it('should support an array of mods.', () => {
-  //     const wrapper = shallow(<Twofold theme={theme} mod={['mod1', 'mod2']} href={href} />);
-  //     expect(wrapper.hasClass(theme.mod1)).to.be.true;
-  //     expect(wrapper.hasClass(theme.mod2)).to.be.true;
-  //   });
-  // });
+  it('should be wrapped in a <div>.', () => {
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
+    expect(wrapper.type()).to.equal('div');
+  });
+
+  it('should contain a clickable <a> tag.', () => {
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
+    expect(wrapper.find('a')).to.have.length(1);
+  });
+
+  it('should contain a header.', () => {
+    theme.header = uniqueid();
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
+    expect(wrapper.find(`.${theme.header}`)).to.have.length(1);
+  });
+
+  it('should contain a heading.', () => {
+    theme.heading = uniqueid();
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
+    expect(wrapper.find(`.${theme.heading}`)).to.have.length(1);
+  });
+
+  it('should contain a subheading.', () => {
+    theme.subheading = uniqueid();
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
+    expect(wrapper.find(`.${theme.subheading}`)).to.have.length(1);
+  });
+
+  it('should contain an icon.', () => {
+    theme.icon = uniqueid();
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
+    expect(wrapper.find(`.${theme.icon}`)).to.have.length(1);
+  });
+
+  it('should contain a detail element.', () => {
+    theme.icon = uniqueid();
+    const wrapper = shallow(<Twofold theme={theme} active={active} />);
+    expect(wrapper.find(`.${theme.detail}`)).to.have.length(1);
+  });
+
+  it('should render its children in the detail element.', () => {
+    const wrapper = shallow(<Twofold theme={theme} active={active}>{child}</Twofold>);
+    expect(wrapper.find(`.${theme.detail}`).contains(child)).to.be.true;
+  });
+
+  describe('when active,', () => {
+
+    it('should have an active class on on the detail.', () => {
+      const wrapper = shallow(<Twofold theme={theme} active>{child}</Twofold>);
+      expect(wrapper.find(`.${theme.detail}`).hasClass(theme.active)).to.be.true;
+    });
+
+    it('should have an open class on on the icon.', () => {
+      theme.iconOpen = uniqueid();
+      const wrapper = shallow(<Twofold theme={theme} active>{child}</Twofold>);
+      expect(wrapper.find(`.${theme.icon}`).hasClass(theme.iconOpen)).to.be.true;
+    });
+  });
+
+  describe('when not active,', () => {
+
+    it('should not have an active class on on the detail.', () => {
+      const wrapper = shallow(<Twofold theme={theme} active={false}>{child}</Twofold>);
+      expect(wrapper.find(`.${theme.detail}`).hasClass(theme.active)).to.be.false;
+    });
+
+    it('should have a closed class on on the icon.', () => {
+      theme.iconClosed = uniqueid();
+      const wrapper = shallow(<Twofold theme={theme} active={false}>{child}</Twofold>);
+      expect(wrapper.find(`.${theme.icon}`).hasClass(theme.iconClosed)).to.be.true;
+    });
+  });
+
+  describe('with a className attribute,', () => {
+    const customClass = uniqueid();
+    theme[customClass] = uniqueid();
+
+    it('should contain that class.', () => {
+      const wrapper = shallow(<Twofold theme={theme} active className={customClass} >{child}</Twofold>);
+      expect(wrapper.hasClass(theme[customClass])).to.be.true;
+    });
+  });
+
+  describe('with a theme with mods,', () => {
+    const themeWithMods = {
+      twofold: uniqueid(),
+      detail: uniqueid(),
+      active: uniqueid(),
+      mod1: uniqueid(),
+      mod2: uniqueid(),
+    };
+
+    it('should support a single mod.', () => {
+      const wrapper = shallow(<Twofold theme={themeWithMods} active mod="mod1" >{child}</Twofold>);
+      expect(wrapper.hasClass(themeWithMods.mod1)).to.be.true;
+    });
+
+    it('should support an array of mods.', () => {
+      const wrapper = shallow(<Twofold theme={themeWithMods} active mod={['mod1', 'mod2']} >{child}</Twofold>);
+      expect(wrapper.hasClass(themeWithMods.mod1)).to.be.true;
+      expect(wrapper.hasClass(themeWithMods.mod2)).to.be.true;
+    });
+  });
 
 });

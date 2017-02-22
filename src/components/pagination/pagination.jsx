78641cb2a6608798ae48bf87bc5587828b1a46e0
Jenny Knuth <jknuth@n.io>
2017-02-22 13:50:34 -0700

    PROD-271 change to stateful component, rough in logic

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -5,77 +5,93 @@ import classNames from 'classnames';
 
 import resolveMods from '../../helpers/resolve-mods';
 
-const Pagination = (props: *) => {
-  const {
-    className: propsClassName,
-    theme,
-    numberOfPages,
-    onChange,
-    active,
-    children,
-    mod,
-    ...rest
-  } = props;
+class Pagination extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      activeArrayStart: 0,
+    };
+  }
 
-  // start on page 1
-  // when number is clicked, active changes to new number
-  // when prev/next is clicked active changes to next number
-  // array changes to prev/next array...
+  render() {
+    const {
+      className: propsClassName,
+      theme,
+      numberOfPages,
+      onChange,
+      active,
+      children,
+      mod,
+      ...rest
+    } = this.props;
 
-  // const changeActive = (page) => {
-  //   console.log(page);
-  // };
-  // create an array of links from all children
-  const pageArray = children.map((url, i) => {
-    const pageNumber = i + 1;
-    const activeClass = (i + 1) === active ? 'active' : null;
-    const linkClasses = classNames(
-      theme.link,
-      theme[activeClass],
-    );
+    const pageArray = children.map((url, i) => {
+      const pageNumber = i + 1;
+      const activeClass = (i + 1) === active ? 'active' : null;
+      const linkClasses = classNames(
+        theme.link,
+        theme[activeClass],
+      );
 
-    const linkProps = {
-      key: pageNumber,
-      className: linkClasses,
-      href: url,
-      onClick: () => onChange(pageNumber),
+      const linkProps = {
+        key: pageNumber,
+        className: linkClasses,
+        href: url,
+        onClick: () => onChange(pageNumber),
+      };
+
+      return (
+        <a {...linkProps} >
+          {i + 1}
+        </a>
+      );
+    });
+
+    const getActiveArray = start => pageArray.slice(start, start + numberOfPages);
+
+    const activeArray = getActiveArray(this.state.activeArrayStart);
+    // console.log(activeArray[0].key);
+
+    const first = activeArray[0].key;
+    const last = activeArray[activeArray.length - 1].key;
+    const min = numberOfPages - first - 1;
+    const prevStart = min > 0 ? min : 0;
+    const nextArray = getActiveArray(last);
+    const prevArray = getActiveArray(prevStart);
+    console.log('first', first);
+    console.log('last', last);
+    console.log('active', active);
+    console.log('next', nextArray);
+    console.log('prev', prevArray);
+
+    const handlePrev = () => {
+      console.log('handleprev start value', prevStart);
+      getActiveArray(prevStart);
+      this.setState({ activeArrayStart: prevStart });
     };
 
-    return (
-      <a {...linkProps} >
-        {i + 1}
-      </a>
+    const handleNext = () => {
+      console.log('handleNext start value', last);
+      getActiveArray(last);
+      this.setState({ activeArrayStart: last });
+    };
+
+    const className = classNames(
+      theme.pagination,
+      resolveMods(theme, mod),
+      theme[propsClassName],
+      ...rest,
     );
-  });
 
-  const getActiveArray = start => pageArray.slice(start, start + numberOfPages);
-
-  const activeArray = getActiveArray(start);
-  const first = activeArray[0];
-  const last = activeArray[activeArray.length - 1];
-  const prevStart = numberOfPages - first.key - 1;
-  const nextArray = getActiveArray(last.key);
-  const prevArray = getActiveArray(prevStart > 0 ? prevStart : 0);
-  console.log(first.key);
-  console.log(last.key);
-  console.log('next', nextArray);
-  console.log('prev', prevArray);
-
-  const className = classNames(
-    theme.pagination,
-    resolveMods(theme, mod),
-    theme[propsClassName],
-    ...rest,
-  );
-
-  return (
-    <div className={className}>
-      <button className={theme.prev} onClick={() => getActiveArray(prevStart > 0 ? prevStart : 0)}>prev</button>
-      {activeArray}
-      <button className={theme.next} onClick={() => getActiveArray(last.key)}>next</button>
-    </div>
-  );
-};
+    return (
+      <div className={className}>
+        <button className={theme.prev} onClick={() => handlePrev(prevStart)}>prev</button>
+        {activeArray}
+        <button className={theme.next} onClick={(last) => handleNext(last)}>next</button>
+      </div>
+    );
+  }
+}
 
 Pagination.defaultProps = {
   numberOfPages: 6,
@@ -99,6 +115,8 @@ Pagination.propTypes = {
     // Base
     pagination: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
+    prev: PropTypes.string.isRequired,
+    next: PropTypes.string.isRequired,
   }).isRequired,
 };
 

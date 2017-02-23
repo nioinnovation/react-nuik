5c55200fbfb3284847a3c7cc6f01535dc723a8de
Jenny Knuth <jknuth@n.io>
2017-02-23 16:33:00 -0700

    PROD-273 add more detailed tests with conditions

--- specs/components/pagination.spec.jsx
+++ specs/components/pagination.spec.jsx
@@ -21,10 +21,49 @@ describe('Pagination', () => {
     expect(wrapper.type()).to.equal('div');
   });
 
+  it('should have a prev and a next icon.', () => {
+    const wrapper = shallow(<Pagination theme={theme}>{data}</Pagination>);
+    expect(wrapper.find('DirectionIcon')).to.have.length(2);
+  });
+
+  it('should contain an array of links.', () => {
+    const wrapper = shallow(<Pagination theme={theme}>{data}</Pagination>);
+    expect(wrapper.find('a')).to.have.length(data.length);
+  });
+
+  it('should contain the number of links contained in its pageRange property.', () => {
+    const wrapper = shallow(<Pagination pageRange={1} theme={theme}>{data}</Pagination>);
+    expect(wrapper.find('a')).to.have.length(1);
+  });
+
+  it('should have exactly one link with an active class.', () => {
+    theme.active = uniqueid();
+    const wrapper = shallow(<Pagination theme={theme}>{data}</Pagination>);
+    expect(wrapper.find(`.${theme.active}`)).to.have.length(1);
+  });
+
   it('should render a page number for each child.', () => {
     const wrapper = shallow(<Pagination theme={theme}>{data}</Pagination>);
     expect(wrapper.contains(0)).to.be.false;
     expect(wrapper.contains(3)).to.be.true;
+  });
+
+  it('should have a DirectionIcon with an href property.', () => {
+    const wrapper = shallow(<Pagination theme={theme}>{data}</Pagination>);
+    expect(wrapper.find('DirectionIcon').first().props().href).to.exist;
+  });
+
+  it('should have a first DirectionIcon with an end property of true and a direction property of "prev".', () => {
+    const wrapper = shallow(<Pagination pageRange={3} theme={theme} >{data}</Pagination>);
+    expect(wrapper.find('DirectionIcon').first().prop('end')).to.be.true;
+    expect(wrapper.find('DirectionIcon').last().prop('end')).to.be.false;
+    expect(wrapper.find('DirectionIcon').first().prop('direction')).to.equal('prev');
+  });
+
+  it('should have a final DirectionIcon with an end property of true and a direction property of "next".', () => {
+    const wrapper = shallow(<Pagination pageRange={uniqueid() * 1} theme={theme} >{data}</Pagination>);
+    expect(wrapper.find('DirectionIcon').last().prop('end')).to.be.true;
+    expect(wrapper.find('DirectionIcon').last().prop('direction')).to.equal('next');
   });
 
   describe('with a className attribute,', () => {
